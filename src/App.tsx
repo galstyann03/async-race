2a487b81bf11778e46694fa1801a33278680e733
Narek Galstyan <narekgalstyanh@gmail.com>
2026-06-03 14:46:27 +0400

    fix: lock navigation during race

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
+import { useAppSelector } from './app/hooks';
 import GaragePage from './features/garage/GaragePage';
 import WinnersPage from './features/winners/WinnersPage';
 import './App.css';
 
 function App() {
+  const isRaceRunning = useAppSelector((state) => state.race.isRaceRunning);
+
+  function blockIfRacing(event: React.MouseEvent<HTMLAnchorElement>) {
+    if (isRaceRunning) event.preventDefault();
+  }
+
+  const linkClass = `app__link${isRaceRunning ? ' app__link--disabled' : ''}`;
+
   return (
     <div className="app">
       <header className="app__header">
         <nav className="app__nav" aria-label="Main navigation">
-          <NavLink to="/" className="app__link" end>
+          <NavLink
+            to="/"
+            className={linkClass}
+            end
+            aria-disabled={isRaceRunning}
+            tabIndex={isRaceRunning ? -1 : 0}
+            onClick={blockIfRacing}
+          >
             Garage
           </NavLink>
-          <NavLink to="/winners" className="app__link">
+          <NavLink
+            to="/winners"
+            className={linkClass}
+            aria-disabled={isRaceRunning}
+            tabIndex={isRaceRunning ? -1 : 0}
+            onClick={blockIfRacing}
+          >
             Winners
           </NavLink>
         </nav>
